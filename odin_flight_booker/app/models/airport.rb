0ac8a94137d7714fb7dc4bf33ddd4679e30cfa78
Brent Cameron <bjcameron@gmail.com>
2020-11-16 13:16:06 -0700

    add info to models for relationship between airport and flight, add correspsonding migrations

--- odin_flight_booker/app/models/airport.rb
+++ odin_flight_booker/app/models/airport.rb
@@ -1,2 +1,5 @@
 class Airport < ApplicationRecord
+    has_many :arriving_flights, class_name: "Flight"
+    has_many :departing_flights, class_name: "Flight"
+
 end
--- odin_flight_booker/db/migrate/20201114214332_create_flights.rb
+++ odin_flight_booker/db/migrate/20201114214332_create_flights.rb
@@ -1,6 +1,9 @@
 class CreateFlights < ActiveRecord::Migration[5.2]
   def change
     create_table :flights do |t|
+      t.datetime :departure_time
+      t.string :departing_airport
+      t.string :arriving_airport
 
       t.timestamps
     end
