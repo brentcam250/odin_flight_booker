19c1f4e6afe32f3e8da295e2d4251773831feee2
Brent Cameron <bjcameron@gmail.com>
2020-11-17 16:43:13 -0700

    add selector for passengers up to 4 people, working on adding date picker

--- odin_flight_booker/app/models/flight.rb
+++ odin_flight_booker/app/models/flight.rb
@@ -2,4 +2,11 @@ class Flight < ApplicationRecord
     belongs_to :to_airport, class_name: "Airport"
     belongs_to :from_airport, class_name: "Airport"
 
+    def flight_date_formatted
+        event_date.strftime("%m/%d/%y")
+    end
+
+    # def get_flight_by_date
+    #     Flight.find_by_sql("select distinct departure_time from flight.departure_time where departure_time is not null order by departure_time desc")
+    # end
 end
