0204a3b34d77cfda079b40a18df6a9fbd148281e
Brent Cameron <bjcameron@gmail.com>
2020-11-17 15:31:30 -0700

    add partial for search form, create drop downs to select from available airports for to/from

--- odin_flight_booker/app/controllers/flights_controller.rb
+++ odin_flight_booker/app/controllers/flights_controller.rb
@@ -5,6 +5,7 @@ class FlightsController < ApplicationController
   # GET /flights.json
   def index
     @flights = Flight.all
+    @airport_options = Airport.all.map{ |a| [a.name, a.id] }
   end
 
   # GET /flights/1
--- odin_flight_booker/db/seeds.rb
+++ odin_flight_booker/db/seeds.rb
@@ -7,11 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-airports = Airport.create({ id: 1, name: "YYC"}, { id: 2, name: "YVR"}, { id: 3, name: "YYZ"})
+airports = Airport.create([{ id: 1, name: "YYC" }, { id: 2, name: "YVR" }, { id: 3, name: "YYZ" }, { id: 4, name: "YEG" }])
 
-flights = Flight.create({ id: 1, departure_time: "2020-12-01 12:30:00", from_airport_id: 1, to_airport_id: 2}, 
+flights = Flight.create([{ id: 1, departure_time: "2020-12-01 12:30:00", from_airport_id: 1, to_airport_id: 2}, 
 { id: 2, departure_time: "2020-12-01 13:30:00", from_airport_id: 2, to_airport_id: 3},
 { id: 3, departure_time: "2020-12-01 14:30:00", from_airport_id: 3, to_airport_id: 1},
 { id: 4, departure_time: "2020-12-01 15:30:00", from_airport_id: 3, to_airport_id: 2},
 { id: 5, departure_time: "2020-12-01 16:30:00", from_airport_id: 2, to_airport_id: 1},
-{ id: 6, departure_time: "2020-12-01 17:30:00", from_airport_id: 1, to_airport_id: 3})
+{ id: 6, departure_time: "2020-12-01 17:30:00", from_airport_id: 1, to_airport_id: 3}])
