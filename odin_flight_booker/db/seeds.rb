42637cb0dcfce79f910c09d74cc733a522ef3852
Brent Cameron <bjcameron@gmail.com>
2020-11-17 11:01:16 -0700

    edit flights index to show useful info, added some db seeds

--- odin_flight_booker/db/seeds.rb
+++ odin_flight_booker/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+airports = Airport.create({ id: 1, name: "YYC"}, { id: 2, name: "YVR"}, { id: 3, name: "YYZ"})
+
+flights = Flight.create({ id: 1, departure_time: "2020-12-01 12:30:00", from_airport_id: 1, to_airport_id: 2}, 
+{ id: 2, departure_time: "2020-12-01 13:30:00", from_airport_id: 2, to_airport_id: 3},
+{ id: 3, departure_time: "2020-12-01 14:30:00", from_airport_id: 3, to_airport_id: 1},
+{ id: 4, departure_time: "2020-12-01 15:30:00", from_airport_id: 3, to_airport_id: 2},
+{ id: 5, departure_time: "2020-12-01 16:30:00", from_airport_id: 2, to_airport_id: 1},
+{ id: 6, departure_time: "2020-12-01 17:30:00", from_airport_id: 1, to_airport_id: 3})
