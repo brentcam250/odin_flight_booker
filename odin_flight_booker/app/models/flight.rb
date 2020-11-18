f1317c624c1e54faef7ccb1cf814ea9694632cc2
Brent Cameron <bjcameron@gmail.com>
2020-11-18 09:43:25 -0700

    date picker showing dates of flights, need to fix bug where date shows up multiple times on dates with multiple flights

--- odin_flight_booker/app/models/flight.rb
+++ odin_flight_booker/app/models/flight.rb
@@ -3,10 +3,10 @@ class Flight < ApplicationRecord
     belongs_to :from_airport, class_name: "Airport"
 
     def flight_date_formatted
-        event_date.strftime("%m/%d/%y")
+        departure_date = departure_time.strftime("%m/%d/%y")
     end
 
-    # def get_flight_by_date
-    #     Flight.find_by_sql("select distinct departure_time from flight.departure_time where departure_time is not null order by departure_time desc")
-    # end
+    def self.get_flight_dates
+        Flight.find_by_sql("select distinct departure_time from flights where departure_time is not null order by departure_time desc")
+    end
 end
