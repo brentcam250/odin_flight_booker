9311842b9e4dd9a720d76dbdedabe9fde961d164
Brent Cameron <bjcameron@gmail.com>
2020-12-01 13:13:29 -0700

    create bookings, passengers, and associations between them

--- odin_flight_booker/app/models/flight.rb
+++ odin_flight_booker/app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
     belongs_to :to_airport, class_name: "Airport"
     belongs_to :from_airport, class_name: "Airport"
+    has_many :bookings
+    has_many :passengers, through: :bookings
 
     def flight_date_formatted
         departure_date = departure_time.strftime("%m/%d/%y")
--- odin_flight_booker/db/migrate/20201126175212_create_bookings.rb
+++ odin_flight_booker/db/migrate/20201126175212_create_bookings.rb
@@ -1,6 +1,7 @@
 class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
+      t.references :flight, null: false, foreign_key: { to_table: :flights }, index: true
 
       t.timestamps
     end
