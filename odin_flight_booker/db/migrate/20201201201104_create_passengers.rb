c1d7058efc46b167bc8cda7af279717f2a67e4cc
Brent Cameron <bjcameron@gmail.com>
2020-12-02 08:56:53 -0700

    edited migrations for new tables... seems to not play nicely with my seeds now

--- odin_flight_booker/db/migrate/20201201201104_create_passengers.rb
+++ odin_flight_booker/db/migrate/20201201201104_create_passengers.rb
@@ -3,6 +3,7 @@ class CreatePassengers < ActiveRecord::Migration[5.2]
     create_table :passengers do |t|
       t.string "name"
       t.string "email"
+      t.references :booking, null: false, foreign_key: { to_table: :bookings }, index: true
 
       t.timestamps
     end
--- odin_flight_booker/db/schema.rb
+++ odin_flight_booker/db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_214340) do
+ActiveRecord::Schema.define(version: 2020_12_01_201104) do
 
   create_table "airports", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -26,6 +33,15 @@ ActiveRecord::Schema.define(version: 2020_11_14_214340) do
     t.datetime "updated_at", null: false
     t.index ["from_airport_id"], name: "index_flights_on_from_airport_id"
     t.index ["to_airport_id"], name: "index_flights_on_to_airport_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
 end
