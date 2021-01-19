5c402744a40fa4712413a21d1bf85fad81330983
Brent Cameron <bjcameron@gmail.com>
2021-01-19 13:27:38 -0700

    add mailer views, mailer

--- odin_flight_booker/app/mailers/passenger_mailer.rb
+++ odin_flight_booker/app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+    default from: 'notifications@example.com'
+    def welcome_email
+        @user = params[:user]
+        @url = 'http://example.com/login'
+        mail(to: @user.email, subject: 'Welcome to Odin Flight Booker!')
+    end
 end
